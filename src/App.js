0a68adedab3276186d930cb54964dd9163a7de25
Saarang Agarwal <agarwalsaarang@yahoo.com>
2021-03-24 12:35:42 -0400

    login page frontend

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import Header from './Header'
 import Home from './Home'
 import Checkout from './Checkout';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './Login';
 
 function App() {
   return (
     //BEM naming convention
     <Router>
       <div className="App">
-        <Header />
+  
         <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route> 
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route> 
 
           <Route path="/">
+            <Header />
             <Home />
           </Route> 
         </Switch>
